392483d8c6737ca610ec8259a751dfffe8da5d23
coolerputt01 <olumideadekolu9@gmail.com>
2025-01-31 10:34:31 +0100

    update

--- script/script.js
+++ script/script.js
@@ -2,30 +2,29 @@ import router from './router.js';
 const { ref, onMounted } = Vue; // Import Vue 3 functions
 const { useIntersectionObserver } = VueUse; // Import VueUse functions
 const Card = {
-  template: `
+      template: `
         <div class="card" ref="card">
           <div class="card-text">
-            <h2 class="card-head">Verified & Secure Apps 🔒</h2>
+            <h2 :class="{'card-head': true, 'border-expand': cardIsVisible}">Verified & Secure Apps 🔒</h2>
             <p class="card-subtext">Every app is scanned and verified for security, ensuring a safe download experience.</p>
           </div>
-          <img src="./images/phonemockup.png" class="card-img">
+          <img src="./images/phonemockup.png" :class="{'card-img':true,'resize':cardIsVisible}">
         </div>
       `,
-  setup() {
-    const card = ref(null);
-    const cardIsVisible = ref(false);
+      setup() {
+        const card = ref(null);
+        const cardIsVisible = ref(false);
 
-    useIntersectionObserver(
-      card,
-      ([entry]) => {
-        cardIsVisible.value = entry.isIntersecting;
-        console.log("Card is visible:", cardIsVisible.value);
-      }
-    );
+        useIntersectionObserver(
+          card,
+          ([entry]) => {
+            cardIsVisible.value = entry.isIntersecting;
+          }
+        );
 
-    return { card };
-  },
-};
+        return { card, cardIsVisible };
+      },
+    };
 const app = Vue.createApp({
   mounted(){
     console.log("Main Vue 'App' is *mounted*");
